c50c41f4da63b10224a301b5f23d0c15730391bc
caiofov <cfoviana@gmail.com>
2022-03-07 19:47:12 -0300

    perfect collision with masks

--- flappybird.py
+++ flappybird.py
@@ -22,7 +22,7 @@ BIRD_IMGS = [
 #pipe images
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 #floor image
-BASE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 #background image
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
@@ -96,6 +96,7 @@ class Bird:
         win.blit(rotated_image, new_rect.topleft) #draws image
 
     def get_mask(self):
+        #mask -> list os pixels of an image
         return pygame.mask.from_surface(self.img)
 
 
@@ -128,7 +129,52 @@ class Pipe:
         win.blit(self.PIPE_TOP, (self.x, self.top))
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
+    def collide(self, bird):
+        #setting masks
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
+        #distance between elements
+        top_offset = (self.x - bird.x, self.top - round(bird.y)) 
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        #checking if they collide
+        #if don't collide -> return None
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True #collides
+
+        return False 
+
+
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+    
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        #when an image is completely out of the screen, we must put this image right before the other one
+        if self.x1 + self.WIDTH < 0: 
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 
 def draw_window(win, bird): #draws the game window
